880d34aa6bbceec19883a1cbf19530f3057c6a89
AyushG <legendsteamprivate@gmail.com>
2024-06-29 16:59:23 +0530

    JavaScript Day1

---
+++ conditions.js
@@ -0,0 +1,9 @@
+n = 345;
+
+if(n%2 === 0)
+    {
+        console.log("Even");
+    }
+    else{
+        console.log("Odd");
+    }
---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Hello javascript');
+console.log('JavaScript is awesome');
+
+a = 637;
+console.log( a );
+console.log( typeof a );
+
+myname = "Ayush"
+console.log( myname );
+console.log( typeof myname );
+
+isLogin = true;
+console.log( isLogin );
