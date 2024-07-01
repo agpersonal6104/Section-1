877a6d9057d8a5c43a7e13e4b76052476e2df47c
AyushG <legendsteamprivate@gmail.com>
2024-07-01 16:34:58 +0530

    array practice js

--- array.js
+++ array.js
@@ -3,3 +3,11 @@ console.log(arr);
 console.log( typeof(arr));
 console.log( Array.isArray(arr) );
 console.log( arr.length );
+console.log('\n\n\n');
+
+const movies = ['300','MI','Deadpool','Expendables','3 Idiots','Inception'];
+
+// indexing
+console.log( movies[1] );
+console.log( movies.indexOf('Inception') );
+console.log( movies.at(-2) );
