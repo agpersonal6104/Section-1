38c5da901413ffeda26fd27f8f7f74badcf46757
AyushG <legendsteamprivate@gmail.com>
2024-07-01 16:57:06 +0530

    JS strings and array

--- array.js
+++ array.js
@@ -2,21 +2,34 @@ const arr= [65,'nice',true, console.log, undefined]
 console.log(arr);
 console.log( typeof(arr));
 console.log( Array.isArray(arr) );
-console.log( arr.length );
+
+
+console.log( arr.length ); //also works with string
 console.log('\n\n\n');
 
 const movies = ['300','MI','Deadpool','Expendables','3 Idiots','Inception'];
 
 // indexing
-console.log( movies[1] );
-console.log( movies.indexOf('Inception') );
-console.log( movies.at(-2) );
+console.log( movies[1] ); //also works with strings
+console.log( movies.indexOf('Inception') );  //also works with strings
+console.log( movies.at(-2) );  //also works with strings
 movies[2] = 'Deadpool and Wolverine';
 console.log(movies);
 
 // slicing
-console.log(movies.slice(2,5));
+console.log(movies.slice(2,5)); //also works with strings
 console.log(movies.slice(2));
 console.log(movies.slice(-4));
 console.log(movies.slice(-4,-2));
 console.log(movies.slice(1,5).reverse());
+console.log(movies);
+
+// String
+console.log(movies[2].slice(-9));
+
+// adding and removing elements
+movies.push('Avengers');  //adds elements at the end
+console.log(movies);
+
+movies.unshift('Kanchana');  //adds element at the beginning
+console.log(movies);
