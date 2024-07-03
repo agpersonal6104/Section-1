1d3f146ccfc63e48416a75e6d1e05e75a556913c
AyushG <legendsteamprivate@gmail.com>
2024-07-03 16:32:42 +0530

    Objects in JS

--- object.js
+++ object.js
@@ -4,3 +4,11 @@ const user = {
     password : 'abc123',
     age : 34
 };
+
+console.log(user);
+console.log(user.name);
+console.log(user['email']);
+
+user.address='Lucknow';
+user.age=22;
+console.log(user);
