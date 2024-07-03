ac772dc0809c2830eaf1cefc4f18aa3da874ff18
AyushG <legendsteamprivate@gmail.com>
2024-07-03 16:59:22 +0530

    objects in JS

--- object.js
+++ object.js
@@ -26,3 +26,30 @@ const key = 'address';
 console.log(user[key]);
 
 console.log('-------------------------------');
+
+console.log( Object.keys(user));
+console.log( Object.values(user));
+console.log( Object.entries(user));
+
+console.log('-------------------------------');
+
+user.contacts = ['7725364123','9131231232'];
+console.log(user.contacts[0]);
+
+console.log('-------------------------------');
+
+const smartphones = {
+    Brand : 'Apple',
+    Model : 'iphone 16',
+    Price : 79999,
+    colors : ['white', 'green', 'black']
+};
+
+// Syntax to access 2nd colour
+console.log(smartphones.colors[1]);
+
+console.log('-------------------------------');
+
+//Replace green with its two variants
+smartphones['colors'].splice(1,1,'Darkgreen','Lightgreen');
+console.log(smartphones.colors);
