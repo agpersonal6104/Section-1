8dfad5b8eef43ccaa6e83b066505a1c277f52f39
AyushG <legendsteamprivate@gmail.com>
2024-07-03 16:40:02 +0530

    Learning Objects in JS

--- object.js
+++ object.js
@@ -9,6 +9,20 @@ console.log(user);
 console.log(user.name);
 console.log(user['email']);
 
+console.log('-------------------------------');
+
 user.address='Lucknow';
 user.age=22;
 console.log(user);
+
+console.log('-------------------------------');
+
+user['roll-no']= 27642;
+console.log(user);
+
+console.log('-------------------------------');
+
+const key = 'address';
+console.log(user[key]);
+
+console.log('-------------------------------');
