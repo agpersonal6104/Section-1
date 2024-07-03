8897e68f66d787026a31a4abd586bc11a4bcbe20
AyushG <legendsteamprivate@gmail.com>
2024-07-03 17:14:25 +0530

    Object is JS

--- object.js
+++ object.js
@@ -42,7 +42,7 @@ const smartphones = {
     Brand : 'Apple',
     Model : 'iphone 16',
     Price : 79999,
-    colors : ['white', 'green', 'black']
+    colors : ['White', 'Green', 'Black']
 };
 
 // Syntax to access 2nd colour
@@ -51,5 +51,45 @@ console.log(smartphones.colors[1]);
 console.log('-------------------------------');
 
 //Replace green with its two variants
-smartphones['colors'].splice(1,1,'Darkgreen','Lightgreen');
+smartphones['colors'].splice(1,1,'Forest green','Sea green');
 console.log(smartphones.colors);
+
+console.log('-------------------------------');
+
+const smartphoneArray = [
+    {
+        Brand : 'Apple',
+        Model : 'iphone 16',
+        Price : 79999,
+        colors : ['White', 'Green', 'Black'],
+        ram : ['6GB','8GB']
+    },
+    {
+        Brand : 'MI',
+        Model : 'poco X2',
+        Price : 16000,
+        colors : ['Grey', 'Black'],
+        ram : ['4GB','16GB']
+    },
+    {
+        Brand : 'Samsung',
+        Model : 'S24 Ultra',
+        Price : 110000,
+        colors : ['Silver', 'Gold'],
+        ram : ['8GB','32GB']
+    },
+    {
+        Brand : 'One Plus',
+        Model : '12R',
+        Price : 36000,
+        colors : ['Pink', 'Blue'],
+        ram : ['64GB','8GB']
+    },
+    {
+        Brand : 'Motorola',
+        Model : 'Edge50',
+        Price : 24000,
+        colors : ['White', 'Black'],
+        ram : ['2GB','8GB']
+    },
+];
